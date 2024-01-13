4b86647ed98f073f148e1df558950666db981216
ramseemapatidar <rampatidar.rs@gmail.com>
2024-01-13 11:46:58 +0530

    submit form using mutation

--- src/components/crud/Add.jsx
+++ src/components/crud/Add.jsx
@@ -1,76 +1,92 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { useMutation,useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
-const addNewData = async({ first_name, last_name,email }) =>{
-    try {
-        const  { data } = await axios.post('http://localhost/react/api/action.php');
-        return data;
-    } catch (error) {
-        throw Error('data not fetching');
-    }
-    
+const addNewData = async ({ first_name, last_name, email }) => {
+	try {
+		const { data } = await axios.post('http://localhost/react/api/action.php');
+		return data;
+	} catch (error) {
+		throw Error('data not fetching');
+	}
+
 }
 export const Add = () => {
-    let navigate = useNavigate();
+	let navigate = useNavigate();
 
 	const queryClient = useQueryClient()
+	const [user, setUser] = useState({
+		first_name : '',
+		last_name : '',
+		email : ''
+	});
 
-	// Mutations
-	const mutation = useMutation({
-		mutationFn: addNewData,
-		onSuccess: () => {
-		  // Invalidate and refetch
-		  queryClient.invalidateQueries({ queryKey: ['addData'] })
-		},
-	  })
-  return (
-    <div className="card">
-			<div className="card-header">
-				<div className="row">
-					<div className="col-md-6">Add User</div>
-					<div className="col-md-6">
-						<Link to="/" className="btn btn-success btn-sm float-end">View All</Link>
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setUser({
+			...user,
+			[name] : value
+		});
+	};
+
+	const handleSubmit = (event) => {
+
+		event.preventDefault();
+		mutate(user)
+		
+	};
+
+	const { mutate,isError } = useMutation({ 
+		mutationFn: (newTodo) => 
+			fetch('http://localhost/react/api/action.php', {
+				method:'POST',
+				body : JSON.stringify(newTodo),
+				headers: {
+					'content-type':'Application/json',
+				},
+			}),
+			onSuccess:async()=>{
+				
+				navigate("/");
+			}
+		
+	})
+	return (
+		<div className="card">
+				<div className="card-header">
+					<div className="row">
+						<div className="col-md-6">Add User</div>
+						<div className="col-md-6">
+							<Link to="/" className="btn btn-success btn-sm float-end">View All</Link>
+						</div>
+					</div>
+				</div>
+				<div className="card-body">
+					<div className="row">
+						<div className="col-md-4">&nbsp;</div>
+						<div className="col-md-4">
+							<form method="POST" onSubmit={handleSubmit}>
+								<div className="mb-3">
+									<label>First Name</label>
+									<input type="text" name="first_name" className="form-control" onChange={handleChange} />
+								</div>
+								<div className="mb-3">
+									<label>Last Name</label>
+									<input type="text" name="last_name" className="form-control" onChange={handleChange} />
+								</div>
+								<div className="mb-3">
+									<label>Email</label>
+									<input type="email" name="email" className="form-control" onChange={handleChange} />
+								</div>
+								<div className="mb-3">
+									<input type="submit" className="btn btn-primary" value="Add" />
+								</div>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div className="card-body">
-				<div className="row">
-					<div className="col-md-4">&nbsp;</div>
-					<div className="col-md-4">
-						
-							<div className="mb-3">
-								<label>First Name</label>
-								<input type="text" name="first_name" className="form-control"  />
-							</div>
-							<div className="mb-3">
-								<label>Last Name</label>
-								<input type="text" name="last_name" className="form-control"  />
-							</div>
-							<div className="mb-3">
-								<label>Email</label>
-								<input type="email" name="email" className="form-control"  />
-							</div>
-							<div className="mb-3">
-								
-		<button
-		className="btn btn-primary"
-        onClick={() => {
-          mutation.mutate({
-            first_name: 'test',
-            last_name: 'Do Laundry',
-			email:'test@gmail.com'
-          })
-        }}
-      >
-        Add Todo
-      </button>
-							</div>
-						
-					</div>
-				</div>
-			</div>
-		</div>
-  )
-}
+	  )
+	}
+
